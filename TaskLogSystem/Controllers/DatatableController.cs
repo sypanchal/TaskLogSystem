f29e3bada7b427a6fcabed182edb66263a038397
sypanchal <saurabh.p@shaligraminfotech.com>
2024-05-16 18:22:17 +0530

    Nested DataTable of Employee's Tasks (Director View)

--- TaskLogSystem/Controllers/DatatableController.cs
+++ TaskLogSystem/Controllers/DatatableController.cs
@@ -67,11 +67,11 @@ namespace TaskLogSystem.Controllers
                 // Search data when search value found
                 if (!string.IsNullOrEmpty(searchValue))
                 {
-                    formattedList = formattedList.Where(x => 
-                    x.TaskName.ToLower().Contains(searchValue.ToLower()) || 
-                    x.TaskDescription.ToLower().Contains(searchValue.ToLower()) || 
-                    x.ApproverName.ToLower().Contains(searchValue.ToLower()) || 
-                    x.ApprovedorRejectedByName.ToLower().Contains(searchValue.ToLower()) || 
+                    formattedList = formattedList.Where(x =>
+                    x.TaskName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.TaskDescription.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApproverName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApprovedorRejectedByName.ToLower().Contains(searchValue.ToLower()) ||
                     x.Status.ToLower().Contains(searchValue.ToLower()));
                 }
 
@@ -80,15 +80,14 @@ namespace TaskLogSystem.Controllers
 
                 // Sort data
                 if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection))
-                {
-                    var sortingExpression = sortColumn + " " + sortColumnDirection;
-                    formattedList = formattedList.OrderBy(sortingExpression);
-                }
+                    formattedList = formattedList.OrderBy(sortColumn + " " + sortColumnDirection);
+                else
+                    formattedList = formattedList.OrderByDescending(t => t.ModifiedOn);
 
                 // Pagination
                 var TaskData = pageSize == -1 ? formattedList.ToList() : formattedList.Skip(skip).Take(pageSize).ToList();
 
-                              //Returning Json Data
+                //Returning Json Data
                 return Json(new { draw, recordsFiltered = filterRecord, recordsTotal = totalRecord, data = TaskData }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
@@ -124,8 +123,8 @@ namespace TaskLogSystem.Controllers
                 int skip = start != null ? Convert.ToInt32(start) : 0;
 
                 // Getting all Employees data for the current user (Manager or Director)
-                var list = Emp.DepartmentID == 2 ? 
-                    _dbContext.Employees.Where(t => t.ReportingPerson == Emp.EmployeeID && !t.IsDeleted).ToList() 
+                var list = Emp.DepartmentID == 2 ?
+                    _dbContext.Employees.Where(t => t.ReportingPerson == Emp.EmployeeID && !t.IsDeleted).ToList()
                     : _dbContext.Employees.Where(t => !t.IsDeleted && t.ReportingPerson != null).ToList();
 
                 // Now perform the formatting and additional processing in-memory
@@ -146,7 +145,7 @@ namespace TaskLogSystem.Controllers
                 // Search data when search value found
                 if (!string.IsNullOrEmpty(searchValue))
                 {
-                    formattedList = formattedList.Where(x => 
+                    formattedList = formattedList.Where(x =>
                     x.EmployeeCode.ToLower().Contains(searchValue.ToLower()) ||
                     x.EmployeeName.ToLower().Contains(searchValue.ToLower()) ||
                     x.Gender.ToLower().Contains(searchValue.ToLower()) ||
@@ -193,7 +192,7 @@ namespace TaskLogSystem.Controllers
 
                 var draw = Request["draw"];
 
-                var sortColumn = Request["columns[" + Request["order[10][column]"] + "][name]"];
+                var sortColumn = Request["columns[" + Request["order[0][column]"] + "][name]"];
                 var sortColumnDirection = Request["order[0][dir]"];
                 var searchValue = Request["search[value]"];
 
@@ -230,13 +229,92 @@ namespace TaskLogSystem.Controllers
                 if (!string.IsNullOrEmpty(searchValue))
                 {
                     formattedList = formattedList.Where(x =>
-                    x.TaskName.ToLower().Contains(searchValue.ToLower()) || 
-                    x.TaskDescription.ToLower().Contains(searchValue.ToLower()) || 
-                    x.ApproverName.ToLower().Contains(searchValue.ToLower()) || 
-                    x.ApprovedorRejectedByName.ToLower().Contains(searchValue.ToLower()) || 
-                    x.Status.ToLower().Contains(searchValue.ToLower()) || 
-                    x.EmployeeName.ToLower().Contains(searchValue.ToLower()) || 
+                    x.TaskName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.TaskDescription.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApproverName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApprovedorRejectedByName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Status.ToLower().Contains(searchValue.ToLower()) ||
+                    x.EmployeeName.ToLower().Contains(searchValue.ToLower()) ||
                     x.EmployeeCode.ToLower().Contains(searchValue.ToLower())
+                    );
+                }
+
+                // Get total count of records after search
+                filterRecord = formattedList.Count();
+
+                // Sort data
+                if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection))
+                {
+                    var sortingExpression = sortColumn + " " + sortColumnDirection;
+                    formattedList = formattedList.OrderBy(sortingExpression);
+                }
+
+                // Pagination
+                var TaskData = pageSize == -1 ? formattedList.ToList() : formattedList.Skip(skip).Take(pageSize).ToList();
+
+                //Returning Json Data
+                return Json(new { draw, recordsFiltered = filterRecord, recordsTotal = totalRecord, data = TaskData }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and return a generic error message
+                //Log.Error("Error occurred while loading data", ex);
+                return Json(new { draw = 0, recordsFiltered = 0, recordsTotal = 0, data = new List<Task>() });
+            }
+        }
+
+        // POST: Datatable/LoadEmployeeTaskData/102
+        [HttpPost]
+        [RoleBasedAuthorizationFilter(1)]
+        public JsonResult LoadEmployeeTaskData(int EmployeeID)
+        {
+            try
+            {
+                int totalRecord = 0;
+                int filterRecord = 0;
+
+                var draw = Request["draw"];
+
+                var sortColumn = Request["columns[" + Request["order[0][column]"] + "][name]"];
+                var sortColumnDirection = Request["order[0][dir]"];
+                var searchValue = Request["search[value]"];
+
+                var start = Request["start"];
+                var length = Request["length"];
+
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+
+                // Getting all Employees data for the current user (Manager or Director)
+                var list = _dbContext.Tasks.Where(t => t.EmployeeID == EmployeeID && !t.IsDeleted).ToList();
+
+                // Now perform the formatting and additional processing in-memory
+                var formattedList = list.Select(t => new
+                {
+                    t.TaskID,
+                    t.TaskName,
+                    t.TaskDescription,
+                    TaskDate = t.TaskDate.Value.ToString("dd MMM yyyy"),
+                    ApproverName = t.Employee1.FirstName + " " + t.Employee1.LastName,
+                    ApprovedorRejectedByName = t.ApprovedorRejectedOn.HasValue ? t.Employee.FirstName + " " + t.Employee.LastName : "Pending",
+                    ApprovedorRejectedOn = t.ApprovedorRejectedOn.HasValue ? t.ApprovedorRejectedOn.Value.ToString("dd MMM yyyy hh:mm:ss") : "Pending",
+                    t.Status,
+                    CreatedOn = t.CreatedOn.HasValue ? t.CreatedOn.Value.ToString("dd MMM yyyy hh:mm:ss") : null,
+                    ModifiedOn = t.ModifiedOn.HasValue ? t.ModifiedOn.Value.ToString("dd MMM yyyy hh:mm:ss") : null
+                }).AsQueryable();
+
+                // Total number of rows count
+                totalRecord = formattedList.Count();
+
+                // Search data when search value found
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    formattedList = formattedList.Where(x =>
+                    x.TaskName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.TaskDescription.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApproverName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.ApprovedorRejectedByName.ToLower().Contains(searchValue.ToLower()) ||
+                    x.Status.ToLower().Contains(searchValue.ToLower())
                     );
                 }
 
